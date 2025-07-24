ec56840f8e990599aaa994bc355e026cade44725
GodzillaCupu <prasetyo.djufri@gmail.com>
2025-07-24 09:17:48 +0700

    Edit Run State : Adding Movement logic and animations logic

--- Assets/_GodzillaCupu/Scripts/FSM/IdleState.cs
+++ Assets/_GodzillaCupu/Scripts/FSM/IdleState.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class IdleState : BaseState
 {
+    PlayerController controller;
+    AnimationsController animations;
+
     public override void OnEnter(StateManager manager)
     {
         // Logic for entering the idle state
+        controller = manager.gameObject.GetComponent<PlayerController>();
+        animations = controller.Animations;
     }
 
     public override void OnUpdate(StateManager manager)
     {
         // Logic for updating the idle state
+        animations.PlayAnimations("Idle");
     }
 
     public override void OnCollisionEnter(Collision other)
