5a5bea8a3cfd9eef042eca48f17ddab8f2bd03c3
GodzillaCupu <prasetyo.djufri@gmail.com>
2025-07-25 14:31:55 +0700

    Fixing Conditions in player controller

--- Assets/_GodzillaCupu/Scripts/Player/Controller/PlayerController.cs
+++ Assets/_GodzillaCupu/Scripts/Player/Controller/PlayerController.cs
@@ -37,8 +37,6 @@ public class PlayerController : InputHandler, IController, IAttack
     // Update is called once per frame
     void Update()
     {
-        if (!CanMove) return;
-
         CheckCanWalk();
         CheckCanJumping();
         OnAttack();
